fc0fcffedeb24426e74c31f97f95418aa48ec6c8
Joel H. <166711728+Artiehen@users.noreply.github.com>
2026-01-15 14:12:47 +0100

    updated with main

--- m01/ex2/ft_plant_growth.py
+++ m01/ex2/ft_plant_growth.py
@@ -1,29 +1,34 @@
-class garden:
-    def __init__(self, name, height, age):
+class Plant:
+    def __init__(self, name, height, age1):
         self.name = name
         self.height = height
-        self.age = age
+        self.age1 = age1
+        self.daily_height = height
+
+    def get_info(self):
+        return f"{self.name}: {self.height}cm, {self.age1} days old "
     
-    def ft_print_garden(self):
-        print(f"{self.name}: {self.height}cm {self.age} days old")
-        
-    def ft_plant_growth(self, other):
-        print(f"Growth this week: +{other.height - self.height}cm" )
+    def grow(self, daily_height):
+        self.height += daily_height
+    
+    def age(self):
+        self.age1 += 1
 
+def main():
+    print("=== Day 1 ===")
+    day1 = Plant("Rose", 50, 20)
+    print(f"{day1.name}: {day1.height}cm {day1.age1} days old")
+    
+    index = 0
+    while index < 7:
+        day1.age()
+        day1.grow(1)
+        index += 1
 
-day1 = garden("Rose", 50, 20)
-day2 = garden("Rose", 51, 21)
-day3 = garden("Rose", 51, 22)
-day4 = garden("Rose", 53, 23)
-day5 = garden("Rose", 54, 24)
-day6 = garden("Rose", 54, 25)
-day7 = garden("Rose", 55, 26)
+    print("=== Day 7 ===")
+    print(day1.get_info())
+    growth = day1.height - day1.daily_height
+    print(f"Growth this week: +{growth}cm")
 
-days = [day1, day2, day3, day4, day5, day6, day7]
-
-l = 7
-index = 0
-while index < l:
-    days[index].ft_print_garden()
-    index += 1
-garden.ft_plant_growth(day1, day7)
+if __name__ == "__main__":
+    main()
