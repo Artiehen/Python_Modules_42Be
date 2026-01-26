fbbaf6e7057a954630f5a0a05af60adbe0a9371c
Artiehen <joel.henriquez4@gmail.com>
2026-01-26 22:29:53 +0100

    ex2m03

--- m03/ex2/ft_coordinate_system.py
+++ m03/ex2/ft_coordinate_system.py
@@ -1,4 +1,3 @@
-import sys
 import math
 
 
@@ -6,42 +5,54 @@ class GeneralError(Exception):
     pass
 
 
+def distance_calculator(pos0, pos1):
+    x0, y0, z0 = pos0
+    x1, y1, z1 = pos1
+    dist = math.sqrt((x1 - x0) ** 2 +
+                     (y1 - y0) ** 2 +
+                     (z1 - z0) ** 2)
+    return dist
+
+
 def coordinate_system():
     try:
-        error_occured = False
-        if len(sys.argv) == 2:
-            xyz0 = "0, 0, 0"
-            xyz1 = sys.argv[1]
-            splitpos1 = xyz1.split(",")
-            splitpos0 = xyz0.split(",")
-            numbers1 = tuple(int(p) for p in splitpos0)
-            numbers2 = tuple(int(p) for p in splitpos1)
-            x01, y01, z01 = numbers2
+        xyz0 = "0, 0, 0"
+        xyz1 = "3,4,0"
+        xyz2 = (10, 20, 5)
+        xyz3 = "abc,def,ghi"
 
-            pos0 = tuple(float(v) for v in splitpos0)
-            pos1 = tuple(float(v) for v in splitpos1)
-            x0, y0, z0 = pos0
-            x1, y1, z1 = pos1
-            dist = math.sqrt((x1 - x0) ** 2 +
-                             (y1 - y0) ** 2 +
-                             (z1 - z0) ** 2)
-            print(f'Parsing coordinates: "{sys.argv[1]}"')
-            print(f"Distance between {numbers1} and {numbers2}: {dist:.2f}")
-        else:
-            error_occured = True
-            raise IndexError
+        splitpos0 = xyz0.split(",")
+        splitpos1 = xyz1.split(",")
+        splitpos3 = xyz3.split(",")
+
+        numbers1 = tuple(int(p) for p in splitpos0)
+        numbers2 = tuple(int(p) for p in splitpos1)
+        x01, y01, z01 = numbers2
+
+        pos0 = tuple(float(v) for v in splitpos0)
+        pos1 = tuple(float(v) for v in splitpos1)
+        pos2 = tuple(float(v) for v in xyz2)
+
+        dist0 = distance_calculator(pos0, pos1)
+        dist1 = distance_calculator(pos0, pos2)
+        print(f"Position created: {xyz2}")
+        print(f"Distance between {numbers1} and {xyz2}: {dist1:.2f}\n")
+
+        print(f'Parsing coordinates: "{xyz1}"')
+        print(f"Parsed position: {numbers2}")
+        print(f"Distance between {numbers1} and {numbers2}: {dist0}\n")
+        numbers3 = tuple(int(p) for p in splitpos3)
+        int(numbers3)
     except ValueError as e:
-        error_occured = True
-        print(f'Parsing invalid coordinates: "{sys.argv[1]}"')
+        print(f'Parsing invalid coordinates: "{xyz3}"')
         print(f"Error parsing coordinate: {e}")
-        print(f'Error details - Type: ValueError, Args:("{e}")')
+        print(f'Error details - Type: ValueError, Args:("{e}",)')
     except IndexError:
         print("Too many arguments")
     finally:
-        if error_occured is False:
-            print("Unpacking demonstration")
-            print(f"Player at x={x01}, y={y01}, z={z01}")
-            print(f"Coordinates: X={x01}, Y={y01}, Z={z01}")
+        print("\nUnpacking demonstration\n")
+        print(f"Player at x={x01}, y={y01}, z={z01}")
+        print(f"Coordinates: X={x01}, Y={y01}, Z={z01}")
 
 
 def main():
@@ -49,7 +60,7 @@ def main():
     try:
         coordinate_system()
     except GeneralError:
-        print("teswt")
+        print("\n")
 
 
 if __name__ == "__main__":
