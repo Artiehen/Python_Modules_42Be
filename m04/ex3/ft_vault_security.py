cff06551ebb1d39af26ce2d330f528fad55fa606
Joel H. <166711728+Artiehen@users.noreply.github.com>
2026-02-04 16:59:50 +0100

    Enhance vault security system with file handling

--- m04/ex3/ft_vault_security.py
+++ m04/ex3/ft_vault_security.py
@@ -1,6 +1,20 @@
-
 def main() -> None:
-    print("=== CYBER ARCHIVES - VAULT SECURITY SYSTEM ===")
+    try:
+        print("=== CYBER ARCHIVES - VAULT SECURITY SYSTEM ===\n")
+        print("Initiating secure vault access...")
+        print("Vault connection established with failsafe protocols\n")
+
+        print("SECURE EXTRACTION")
+        with open("classified_data.txt", "r") as file:
+            print(file.read())
+        print("\nSECURE PRESERVATION")
+        with open("security_protocols.txt", "r") as file1:
+            print(file1.read())
+
+        print("\nVault automatically sealed upon completion")
+        print("All vault operations completed with maximum security.")
+    except FileNotFoundError as e:
+        print(e)
 
 
 if __name__ == "__main__":
