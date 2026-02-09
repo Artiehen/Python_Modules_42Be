b6c3541073b2c09a11309bfe69f1d8296deee299
Joel H. <166711728+Artiehen@users.noreply.github.com>
2026-02-09 15:18:45 +0100

    Small changes

--- m05/ex1/testclases.py
+++ m05/ex1/testclases.py
@@ -5,116 +5,31 @@ class DataStream(ABC):
 
     def __init__(self, stream_id: str) -> None:
         self.stream_id = stream_id
-
-    # @abstractmethod
-    # def process_batch(self, data_batch: List[Any]) -> str:
-    #     """
-    #     Process a batch of data.
-    #     Must be implemented by subclasses.
-    #     """
-    #     pass
-
-    # def filter_data(self, data_batch: List[Any], criteria: Optional[str] = None) -> List[Any]:
-    #     """
-    #     Default filtering behavior.
-    #     Subclasses may override.
-    #     """
-    #     if criteria is None:
-    #         return data_batch
-
-    #     return [item for item in data_batch if criteria in str(item)]
-
-    # def get_stats(self) -> Dict[str, Union[str, int, float]]:
-    #     pass
+        self.processed_batches = 0
+        self.total_items = 0
 
 
 class SensorStream(DataStream):
     def initialize(self) -> None:
-        print("Initializing Sensor Stream...")
-        print(f"Stream ID: {self.stream_id}, Type Environmental Data\n")
+        return (
+            "Initializing Sensor Stream...\n"
+            f"Stream ID: {self.stream_id}, Type Environmental Data\n"
+        )
 
-    # def process_batch(self, data_batch: List[Any]) -> str:
-    #     if not data_batch:
-    #         return "No sensor data"
-
-    #     try:
-    #         values: List[float] = []
-    #         readings: List[str] = []
-
-    #         for item in data_batch:
-    #             for key in ("temperature", "humidity", "pressure"):
-    #                 if key in item:
-    #                     values.append(float(item[key]))
-    #                     readings.append(f"{key}: {item[key]}")
-
-    #     except (TypeError, ValueError):
-    #         raise ValueError("SensorStream requires numeric sensor values")
-    #     print(f"processing sensor batch: {readings}")
-    #     self.processed_batches += 1
-    #     self.total_items += len(values)
-
-    #     # average = sum(values) / len(values)
-    #     return f"{readings}\n"
-
-    # def get_stats(self) -> Dict[str, Union[str, int]]:
-    #     stats = super().get_stats()
-    #     # stats["type"] = "Environmental Data"
-    #     return stats
-
-    # def filter_data(self, data_batch: List[Any], criteria: Optional[str] = None) -> List[Any]:
-    #     return data_batch
-    
-# class TransactionStream(DataStream):
-#     def initialize(self) -> None:
-#         print("Initializing Transaction Stream...")
-#         print(f"Stream ID: {self.stream_id}, Type Financial Data\n")
-    
-#     def process_batch(self, data_batch: List[Any]) -> str:
-#         try:
-#             amounts = [item["amount"] for item in data_batch]
-#         except (TypeError, KeyError):
-#             raise ValueError("Invalid transaction data format")
-        
-#         self.processed_batches += 1
-#         self.total_items += len(amounts)
-        
-#         total = sum(amounts)
-#         return f"Total transaction amount: {total:.2f}"
-    
-#     def filter_data(self, data_batch: List[Any], criteria: Optional[str] = None) -> List[Any]:
-#         if criteria is None:
-#             return data_batch
-#         return [tx for tx in data_batch if tx.get("type") == criteria]
+class TransactionStream(DataStream):
+    def initialize(self) -> None:
+        return (
+            "Initializing Transaction Stream...\n"
+            f"Stream ID: {self.stream_id}, Type Financial Data\n"
+        )
 
 
-# class EventStream(DataStream):
-#     def initialize(self) -> None:
-#         print("Initializing Event Stream...")
-#         print(f"Stream ID: {self.stream_id}, Type Event Data\n")
-#     def process_batch(self, data_batch: List[Any]) -> str:
-#         if not all(isinstance(event, str) for event in data_batch):
-#             raise ValueError("EventStream requires string events")
-
-#         self.processed_batches += 1
-#         self.total_items += len(data_batch)
-
-#         return f"Processed {len(data_batch)} events"
-
-#     def filter_data(self, data_batch: List[Any], criteria: Optional[str] = None) -> List[Any]:
-#         if criteria is None:
-#             return data_batch
-
-#         return [
-#             event for event in data_batch
-#             if event.lower().startswith(criteria.lower())
-#         ]
-
-#     def get_stats(self) -> Dict[str, Union[str, int, float]]:
-#         stats = super().get_stats()
-#         # stats["type"] = "EventStream"
-#         return stats
-
-    
+class EventStream(DataStream):
+    def initialize(self) -> None:
+        return (
+            "Initializing Event Stream...\n"
+            f"Stream ID: {self.stream_id}, Type Event Data\n"
+        )
 
 
 
@@ -123,28 +38,13 @@ def main() -> None:
     SENSOR_001 = {"SENSOR_001": [{"temperature": 22.5}, {"humidity": 21.8}, {"pressure": 23.1}]}
     TRANS_001 = {"tx_1": [{"amount": 100.0, "type": "credit"}, {"amount": 50.0, "type": "debit"}, {"amount": 75.0, "type": "credit"}]}
     event_001 = {"event_1": ["LoginSuccess", "LoginFailure", "Logout"]}
-    sensor_stream = SensorStream("SENSOR_001")
-    # transaction_stream = TransactionStream("TRANS_001")
-    # event_stream = EventStream("EVENT_001")
-
-    # processor = StreamProcessor()
-    # processor.add_stream(sensor_stream)
-    # processor.add_stream(transaction_stream)
-    # processor.add_stream(event_stream)
-
-
-
-
-
-    # mixed_batches = {
-    #     "SENSOR_001": [{"temperature": 22.5}, {"humidity": 21.8}, {"pressure": 23.1}],
-    #     "tx_1": [{"amount": 100.0, "type": "credit"}, {"amount": 50.0, "type": "debit"}, {"amount": 75.0, "type": "credit"}],
-    #     "event_1": ["LoginSuccess", "LoginFailure", "Logout"]
-    # }
+    sensor_stream = SensorStream(SENSOR_001)
+    transaction_stream = TransactionStream("TRANS_001")
+    event_stream = EventStream("EVENT_001")
 
     print(f"{sensor_stream.initialize()}")
-    # print(f"{transaction_stream.stream_id}")
-    # print(f"{event_stream.stream_id}")
+    print(f"{transaction_stream.initialize()}")
+    print(f"{event_stream.initialize()}")
 
 if __name__ == "__main__":
     main()
