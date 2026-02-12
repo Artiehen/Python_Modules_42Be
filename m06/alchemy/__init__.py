ea3ad59320447e60f875288d43973a2f3a3fa8b5
Joel H. <166711728+Artiehen@users.noreply.github.com>
2026-02-12 15:14:57 +0100

    Start m06

---
+++ m06/alchemy/__init__.py
@@ -0,0 +1,9 @@
+__version__ = "1.0.0"
+__author__ = "Master Pythonicus"
+
+from .elements import create_fire, create_water
+
+print("=== Sacred Scroll Mastery ===")
+
+print("Testing direct module access:")
+print("alchemy.elements.create_fire():", elements.create_fire())
