132175ff40c02836f482dec06f3532cce083a501
Joel Henriquez <joel.henriquez4@gmail.com>
2026-02-18 15:36:55 +0100

    fixed __init__

--- m06/alchemy/__init__.py
+++ m06/alchemy/__init__.py
@@ -5,4 +5,3 @@ __author__ = "Master Pythonicus"
 
 from .elements import create_fire, create_water
 
-__all__ = ["create_fire", "create_water"]
--- m06/alchemy/grimoire/__init__.py
+++ m06/alchemy/grimoire/__init__.py
@@ -1,4 +1,2 @@
 from .spellbook import record_spell
 from .validator import validate_ingredients
-
-__all__ = ["record_spell", "validate_ingredients"]
