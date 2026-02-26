ad49b648a11411b6f67f82b6c6e6a6ce3fd94f46
Joel H. <166711728+Artiehen@users.noreply.github.com>
2026-02-26 17:49:11 +0100

    Ex02 work in progress
    Load environment variables from .env.example and handle errors.

--- m08/ex02/oracle.py
+++ m08/ex02/oracle.py
@@ -1,1 +1,26 @@
+import os
 
+
+def main():
+    try:
+        from dotenv import load_dotenv
+    except ImportError as e:
+        print(e)
+    try:
+        load_dotenv('.env.example')
+    except FileNotFoundError as e:
+        print(e)
+    print("\nORACLE STATUS: Reading the Matrix...\n")
+    keys = ['MATRIX_MODE', 'DATABASE_URL', 'API_KEY', 'LOG_LEVEL',
+            'ZION_ENDPOINT']
+    for info in keys:
+        res = os.getenv(info)
+        if res is None:
+            print(f"Could not get the environment key for {info}.")
+            print("Please check environment variables")
+            exit(1)
+    print("Configuration loaded:")
+
+
+if __name__ == "__main__":
+    main()
